15d3937269571a07ac8894d6128a73647af1f850
vipermagic <vipermagi@yandex.ru>
2017-11-09 07:20:06 +1000

    Make 1 main task

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,6 +1,9 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealWithExceed;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,16 +11,35 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class MealServlet extends HttpServlet {
     private static final Logger log = getLogger(MealServlet.class);
 
+    private List<Meal> meals = Arrays.asList(
+            new Meal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
+            new Meal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000),
+            new Meal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500),
+            new Meal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000),
+            new Meal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
+            new Meal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
+    );
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("redirect to meals");
 
-        response.sendRedirect("meals.jsp");
+        List<MealWithExceed> mealsWithExceed = MealsUtil.getFilteredWithExceeded(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+
+        request.setAttribute("list", mealsWithExceed);
+
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
+//        response.sendRedirect("meals.jsp");
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,4 +1,5 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
     <title>Meals</title>
@@ -6,5 +7,34 @@
 <body>
 <h3><a href="index.html">Home</a></h3>
 <h2>Meals</h2>
+
+<style type="text/css">
+    .exceeded_meal{color:red}
+    .normal_meal{color:green}
+</style>
+
+<table class="tftable" border="1">
+    <thead><tr><th>Description</th><th>Date</th><th>Calories</th></tr></thead>
+    <tbody>
+        <c:forEach var="meal" items="${list}">
+
+            <tr>
+
+                <c:if test="${meal.exceed == true}">
+                    <th><p class="exceeded_meal">${meal.description}</p></th>
+                    <th><p class="exceeded_meal">${meal.dateTime}</p></th>
+                    <th><p class="exceeded_meal">${meal.calories}</p></th>
+                </c:if>
+                <c:if test="${meal.exceed == false}">
+                    <th><p class="normal_meal">${meal.description}</p></th>
+                    <th><p class="normal_meal">${meal.dateTime}</p></th>
+                    <th><p class="normal_meal">${meal.calories}</p></th>
+                </c:if>
+
+            </tr>
+        </c:forEach>
+    </tbody>
+</table>
+
 </body>
 </html>
